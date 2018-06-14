ff51ab30033248630e2f80f2fe3dc38425355bd8
ILdar Shamiev <shamief@yandex.ru>
2018-06-14 18:15:02 +0300

    w3 sol2 tests pass, the higher priority has a worker with a smaller index

--- solutions/w3/job_queue.py
+++ solutions/w3/job_queue.py
@@ -13,6 +13,9 @@ class JobQueue:
         self.num_workers, m = map(int, input().split())
         self.next_free_time = [(0, -1)] * self.num_workers  # next time and number of worker
         self.jobs = list(map(int, input().split()))
+        len_jobs = len(self.jobs)
+        self.assigned_workers = [None] * len_jobs
+        self.start_times = [None] * len_jobs
         assert m == len(self.jobs)
 
     def set_data(self, jobs, workers):
@@ -23,8 +26,12 @@ class JobQueue:
         :return: None
         """
         self.num_workers = workers
+        self.next_worker = -1
         self.next_free_time = [(0, -1)] * self.num_workers
         self.jobs = jobs
+        len_jobs = len(self.jobs)
+        self.assigned_workers = [None] * len_jobs
+        self.start_times = [None] * len_jobs
 
     def get_answer(self):
         """
@@ -58,8 +65,6 @@ class JobQueue:
         Faster implementation with priority queue (min-heap binary tree).
         """
         len_jobs = len(self.jobs)
-        self.assigned_workers = [None] * len_jobs
-        self.start_times = [None] * len_jobs
 
         for i in range(len_jobs):
 
@@ -89,10 +94,16 @@ class JobQueue:
         size = len(self.next_free_time)
         min_index = i
         l = self.left_child(i)
-        if l <= size - 1 and self.next_free_time[l][0] < self.next_free_time[min_index][0]:
+        # jobs with a shorter execution time have a higher priority
+        # if the execution time is the same, then the priority has a worker with a smaller index
+        if l <= size - 1 and (self.next_free_time[l][0] < self.next_free_time[min_index][0] or (
+                self.next_free_time[l][0] == self.next_free_time[min_index][0] and
+                self.next_free_time[l][1] < self.next_free_time[min_index][1])):
             min_index = l
         r = self.right_child(i)
-        if r <= size - 1 and self.next_free_time[r][0] < self.next_free_time[min_index][0]:
+        if r <= size - 1 and (self.next_free_time[r][0] < self.next_free_time[min_index][0] or (
+                self.next_free_time[r][0] == self.next_free_time[min_index][0] and
+                self.next_free_time[r][1] < self.next_free_time[min_index][1])):
             min_index = r
         if i != min_index:
             self.next_free_time[i], self.next_free_time[min_index] = (self.next_free_time[min_index],
@@ -113,7 +124,9 @@ class JobQueue:
         self.sift_up(len(self.next_free_time) - 1)
 
     def sift_up(self, i):
-        while i > 0 and self.next_free_time[self.parent(i)][0] > self.next_free_time[i][0]:
+        while i > 0 and (self.next_free_time[self.parent(i)][0] > self.next_free_time[i][0] or (
+                self.next_free_time[self.parent(i)][0] == self.next_free_time[i][0] and
+                self.next_free_time[self.parent(i)][1] > self.next_free_time[i][1])):
             _parent = self.parent(i)
             self.next_free_time[_parent], self.next_free_time[i] = self.next_free_time[i], self.next_free_time[_parent]
             i = _parent
