e6bdbaef32f8ffa9ee502662b11247732e1e156f
ILdar Shamiev <shamief@yandex.ru>
2018-06-26 16:11:04 +0300

    w6 sol2 in_order traversal of tree, grader tests passed

--- solutions/w6/is_bst.py
+++ solutions/w6/is_bst.py
@@ -9,7 +9,27 @@ threading.stack_size(2**25)  # new thread will get stack of such size
 
 def is_bst(tree):
     # Implement correct algorithm here
-    return True
+    keys = []
+    return in_order_recursive(tree, 0, keys) if tree else not tree
+
+
+def in_order_recursive(tree, vertex, keys):
+    """
+    Traversing a tree from left to right and
+    comparing neighboring keys in pairs.
+    :param tree: list of vertices (list of key with left and right children)
+    :param vertex: index in tree
+    :param keys: list of keys
+    :return: True if tree is bst, else False
+    """
+    if vertex == -1:
+        return True
+    result = in_order_recursive(tree, tree[vertex][1], keys)
+    if not result or (keys and keys[-1] > tree[vertex][0]):
+        return False
+    keys.append(tree[vertex][0])
+    result = in_order_recursive(tree, tree[vertex][2], keys)
+    return result
 
 
 def main():
